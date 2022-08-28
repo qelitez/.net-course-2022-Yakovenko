d60d6e8df20bee63aca840544236bcf2594e755a
qelitez <v0vande2001@gmail.com>
2022-08-28 19:18:12 +0300

    added new method for currency

--- PracticeWithTypes/PracticeWithTypes.cs
+++ PracticeWithTypes/PracticeWithTypes.cs
@@ -12,16 +12,35 @@ namespace PracticeWithTypes
             emp1.BirthDate = DateTime.Now;
             emp1.PassNumber = 123456;
 
+            Currency currency = new Currency();
+            currency.code = 240;
+            currency.value = "EUR";
+
             NewContact(emp1);
 
             Console.WriteLine("Наш контракт: " + emp1.Contract);
 
+            currency = UpdateCurrency(currency); // меняет свойства валюты
+            Console.WriteLine("Изначально используется валюта EUR, пробуем поменять. Результат " + currency.value);
+
         }
         public static void NewContact(Employee newEmployee)
         {
+            Console.WriteLine("Введите новое значение контракта");
             newEmployee.Contract = Console.ReadLine();
         }
+
+        static Currency UpdateCurrency(Currency currency)
+        {
+            Console.WriteLine("Введите новый код валюты");
+            //currency.code = 120;
+            currency.code = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите валюту");
+            //currency.value = "USD";
+            currency.value = Console.ReadLine();
+
+            return currency;
+        }
+
     }
-
-
 }
