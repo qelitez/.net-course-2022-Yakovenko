cdb10f7b65b31d493303eac69052d6f1488355fb
qelitez <v0vande2001@gmail.com>
2022-08-28 18:11:12 +0300

    Practice with Types

---
+++ Models/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace Models
+{
+    public class Person
+    {
+        
+        private string name = "Введите имя";
+        private DateTime birthDate;
+        private int passNumber;
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public int PassNumber
+        {
+            get { return passNumber; }
+            set { passNumber = value; }
+        }
+
+    }
+}
---
+++ PracticeWithTypes/PracticeWithTypes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Models;
+
+namespace PracticeWithTypes
+{
+    public class PracticeWithTypes
+    {
+        public static void Main()
+        {
+            Employee emp1 = new Employee();
+            emp1.Name = "Fedor";
+            emp1.BirthDate = DateTime.Now;
+            emp1.PassNumber = 123456;
+
+            NewContact(emp1);
+
+            Console.WriteLine("Наш контракт: " + emp1.Contract);
+
+        }
+        public static void NewContact(Employee newEmployee)
+        {
+            newEmployee.Contract = Console.ReadLine();
+        }
+    }
+
+
+}
